b6267bcd49f112aea4e976377e7d13356bf36430
CunyangShui <shui-MBP@Cunyangs-MacBook-Pro-4.local>
2017-02-07 16:23:34 -0500

    init constructors

---
+++ Constructors/Constructors.cpp
@@ -0,0 +1,23 @@
+///////////////////////////////////////////////////////////////////////
+// Callables.cpp - Executing Callable Objects                        //
+//                                                                   //
+// Author: Cunyang Shui, shuicunyang@gmail.com                       //
+// Source: Jim Fawcett, CSE687 - Object Oriented Design              //
+///////////////////////////////////////////////////////////////////////
+//
+// demo of callable object
+//
+
+
+#include <iostream>
+#include "../Utilities/Utilities.h"
+
+using namespace Utilities;
+using Utils = StringHelper;
+
+int main(){
+  Utils::Title(" I will be constructor demo");
+
+  std::cout << "\n\n";
+
+}
